22bc4886db35323bd7732cdefd28c7d5819a0c97
Jai Singh <thakurjk825@gmail.com>
2026-01-03 22:54:14 +0530

    Add files via upload

---
+++ 03_list_using_while.py
@@ -0,0 +1,6 @@
+l=[1,"Harry",False,"This","rohan","Shubham","Shubhi"]
+
+i=0
+while(i<len(1)):
+    print(l[i])
+    i +=1
---
+++ 05_for_loop_iterate.py
@@ -0,0 +1,15 @@
+#FOR loop with lists
+# l=[1,4,6,234,6,764]
+
+# for i in l:
+#     print(i)
+
+#for loops in tuples
+# t=(6,231,75,122)
+# for i in t:
+#     print(i)
+
+#for loops in strings
+s="Harry"
+for i in s:
+    print(i)
